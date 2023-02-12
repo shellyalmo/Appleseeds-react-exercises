e932b1a8a87ac8738ef9bdaba1b3bc5674f8f1cc
shellyalmo <shellyalmo@gmail.com>
2023-02-12 18:00:23 +0200

    ex 4.1

---
+++ ex-4.1/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Box1 from "./components/Box1";
+
+function App() {
+  return (
+    <div>
+      <Box1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ ex-4.1/src/components/Box3.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Box4 from "./Box4";
+
+const Box3 = () => {
+  return (
+    <div className="box3">
+      <Box4 className="box4" />
+      <Box4 className="box4" />
+    </div>
+  );
+};
+
+export default Box3;
