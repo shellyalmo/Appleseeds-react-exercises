310dada62a762ab9696f0d964018ee20e627996e
shellyalmo <shellyalmo@gmail.com>
2023-02-20 11:55:02 +0200

    ex13.2

---
+++ ex-13.2/src/App.js
@@ -0,0 +1,13 @@
+import Box from "./components/Box";
+
+function App() {
+  return (
+    <div className="container">
+      <Box width={300} height={100} />
+      <Box width={200} height={100} />
+      <Box width={100} height={100} />
+    </div>
+  );
+}
+
+export default App;
